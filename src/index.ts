ddfd51a404c0ff6ad0ca9d189a4de960cab46ced
Nishant Mittal <mittalnishant14@outlook.com>
2021-06-13 15:34:29 +0530

    feat: store templates in a special folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import joplin from 'api';
+import joplin from "api";
+import { SettingItemType } from "api/types";
+import { doesFolderExist } from './utils';
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
+		// TODO: The settings name i.e. "templatesFolderId" in this case should be stored in a variable.
+		await joplin.settings.registerSettings({
+			"templatesFolderId": {
+				public: false,
+				type: SettingItemType.String,
+				value: null,
+				label: "Templates folder ID"
+			}
+		});
+
+		const templatesFolderId = await joplin.settings.value("templatesFolderId");
+		if (templatesFolderId == null || !(await doesFolderExist(templatesFolderId))) {
+			const folder = await joplin.data.post(["folders"], null, { title: "Templates" });
+			await joplin.settings.setValue("templatesFolderId", folder.id);
+		}
 	},
 });
