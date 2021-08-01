17869fe8ff8b0a18065bbd99245be4cadaf5602b
Nishant Mittal <mittalnishant14@outlook.com>
2021-08-01 12:25:34 +0530

    refactor: remove unused code

--- src/utils/folders.ts
+++ src/utils/folders.ts
@@ -1,19 +1,4 @@
 import joplin from "api";
-import { fetchAllItems } from "./dataApi";
-import { Note } from "./templates";
-
-export const doesFolderExist = async (folderId: string): Promise<boolean> => {
-    try {
-        await joplin.data.get([ "folders", folderId ], { fields: ["title"] });
-        return true;
-    } catch (error) {
-        return false;
-    }
-};
-
-export const getAllNotesInFolder = async (folderId: string): Promise<Note[]> => {
-    return fetchAllItems(["folders", folderId, "notes"], { fields: ["id", "title", "body"] });
-}
 
 export const createFolder = async (title: string): Promise<string> => {
     const folder = await joplin.data.post(["folders"], null, { title: title });
