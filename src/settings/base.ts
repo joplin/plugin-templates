b63dc77b1a8726309472086c36dbe194de09fada
Nishant Mittal <mittalnishant14@outlook.com>
2024-09-29 07:05:16 +0530

    fix: use the new settings framework for templates source

--- src/settings/base.ts
+++ src/settings/base.ts
@@ -23,3 +23,24 @@ export const createSimpleSetting = <T>(id: string, manifest: SettingItem): Plugi
         }
     }
 }
+
+/**
+ * This considers that the original setting is of type string. On `set` if no original value
+ * can be calculated, the setting is set to an empty string.
+ */
+export const createEnumSetting = <T>(id: string, manifest: SettingItem, valueMap: Record<string, T>, defaultValue: T): PluginSetting<T> => {
+    return class {
+        static id = id;
+        static manifest = manifest;
+
+        static async get(): Promise<T> {
+            const value: string = await joplin.settings.value(id);
+            return value in valueMap ? valueMap[value] : defaultValue;
+        }
+
+        static async set(newValue: T): Promise<void> {
+            const potentialValues = Object.entries(valueMap).filter((entry) => entry[1] === newValue);
+            await joplin.settings.setValue(id, potentialValues.length ? potentialValues[1][0] : "" );
+        }
+    }
+}
--- src/settings/templatesSource.ts
+++ src/settings/templatesSource.ts
@@ -1,7 +1,19 @@
 import { SettingItemType } from "api/types";
-import { createSimpleSetting } from "./base";
+import { createEnumSetting } from "./base";
 
-export const TemplatesSourceSetting = createSimpleSetting<string>("templatesSource", {
+export enum TemplatesSource {
+    Tag,
+    Notebook,
+}
+
+const createValueMap = (): Record<string, TemplatesSource> => {
+    const valueMap: Record<string, TemplatesSource> = {};
+    valueMap["tag"] = TemplatesSource.Tag;
+    valueMap["notebook"] = TemplatesSource.Notebook;
+    return valueMap;
+}
+
+export const TemplatesSourceSetting = createEnumSetting<TemplatesSource>("templatesSource", {
     public: true,
     type: SettingItemType.String,
     isEnum: true,
@@ -13,4 +25,4 @@ export const TemplatesSourceSetting = createSimpleSetting<string>("templatesSour
     label: "Are templates set with tags or stored in a notebook?",
     description: "If set to 'Tag', any note/to-do with a 'template' tag is considered a template. If set to 'Notebook', any note/todo stored in a notebook titled 'Templates' is considered a template.",
     section: "templatesPlugin"
-});
+}, createValueMap(), TemplatesSource.Tag);
