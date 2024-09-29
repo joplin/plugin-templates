e45199eefb5b1cd07e9cfd94d119bb3afe622f6f
Nishant Mittal <mittalnishant14@outlook.com>
2024-09-29 07:10:18 +0530

    fix: remove unnecessary code

--- src/settings/templatesSource.ts
+++ src/settings/templatesSource.ts
@@ -6,13 +6,6 @@ export enum TemplatesSource {
     Notebook,
 }
 
-const createValueMap = (): Record<string, TemplatesSource> => {
-    const valueMap: Record<string, TemplatesSource> = {};
-    valueMap["tag"] = TemplatesSource.Tag;
-    valueMap["notebook"] = TemplatesSource.Notebook;
-    return valueMap;
-}
-
 export const TemplatesSourceSetting = createEnumSetting<TemplatesSource>("templatesSource", {
     public: true,
     type: SettingItemType.String,
@@ -25,4 +18,4 @@ export const TemplatesSourceSetting = createEnumSetting<TemplatesSource>("templa
     label: "Are templates set with tags or stored in a notebook?",
     description: "If set to 'Tag', any note/to-do with a 'template' tag is considered a template. If set to 'Notebook', any note/todo stored in a notebook titled 'Templates' is considered a template.",
     section: "templatesPlugin"
-}, createValueMap(), TemplatesSource.Tag);
+}, { "tag": TemplatesSource.Tag, "notebook": TemplatesSource.Notebook }, TemplatesSource.Tag);
