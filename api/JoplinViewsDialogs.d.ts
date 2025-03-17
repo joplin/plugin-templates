13134a481eb528560ee973d2f47b009ce3e6e9b1
alondmnt <alondmnt@users.noreply.github.com>
2025-03-17 23:24:25 +1100

    chore(api): update api

--- api/JoplinViewsDialogs.d.ts
+++ api/JoplinViewsDialogs.d.ts
@@ -1,5 +1,5 @@
 import Plugin from '../Plugin';
-import { ButtonSpec, ViewHandle, DialogResult } from './types';
+import { ButtonSpec, ViewHandle, DialogResult, Toast } from './types';
 /**
  * Allows creating and managing dialogs. A dialog is modal window that
  * contains a webview and a row of buttons. You can update the
@@ -43,6 +43,15 @@ export default class JoplinViewsDialogs {
      * Displays a message box with OK/Cancel buttons. Returns the button index that was clicked - "0" for OK and "1" for "Cancel"
      */
     showMessageBox(message: string): Promise<number>;
+    showToast(toast: Toast): Promise<void>;
+    /**
+     * Displays a dialog to select a file or a directory. Same options and
+     * output as
+     * https://www.electronjs.org/docs/latest/api/dialog#dialogshowopendialogbrowserwindow-options
+     *
+     * <span class="platform-desktop">desktop</span>
+     */
+    showOpenDialog(options: any): Promise<any>;
     /**
      * Sets the dialog HTML content
      */
@@ -56,13 +65,14 @@ export default class JoplinViewsDialogs {
      */
     setButtons(handle: ViewHandle, buttons: ButtonSpec[]): Promise<ButtonSpec[]>;
     /**
-     * Opens the dialog
+     * Opens the dialog.
+     *
+     * On desktop, this closes any copies of the dialog open in different windows.
      */
     open(handle: ViewHandle): Promise<DialogResult>;
     /**
      * Toggle on whether to fit the dialog size to the content or not.
-     * When set to false, the dialog stretches to fill the application
-     * window.
+     * When set to false, the dialog is set to 90vw and 80vh
      * @default true
      */
     setFitToContent(handle: ViewHandle, status: boolean): Promise<boolean>;
