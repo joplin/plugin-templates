3873d00b9318651ac015fd587b40dcfe490bab79
ShivamChaudhary <sc31122004@gmail.com>
2026-02-25 01:50:05 +0530

    fix: use type=text for number inputs to avoid Mac M1 Chromium bug

--- src/variables/types/number.ts
+++ src/variables/types/number.ts
@@ -6,7 +6,11 @@ export class NumberCustomVariable extends CustomVariable {
     static definitionName = "number";
 
     protected inputHTML(): string {
-        return `<input name="${encode(this.name)}" type="number"></input>`;
+        // Using type="text" with inputmode="decimal" instead of type="number"
+        // to avoid a Mac M1 / Chromium bug where native number inputs silently
+        // replace typed values with different numbers.
+        // See: https://github.com/joplin/plugin-templates/issues/143
+        return `<input name="${encode(this.name)}" type="text" inputmode="decimal" pattern="-?[0-9]*\\.?[0-9]*" placeholder="Enter a number"></input>`;
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
